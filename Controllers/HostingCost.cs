b94ac8a75d77ae7e043181497b7ef9304b760879
Ambaram <34491921+Ambaram@users.noreply.github.com>
2021-02-16 23:55:36 +0530

    Update HostingCost.cs

--- Controllers/HostingCost.cs
+++ Controllers/HostingCost.cs
@@ -1,1 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class HostingCostController : ApiController
+    {
+        /// <summary>
+        /// Outputs 3 strings which describe the total hosting cost using GET method
+        /// <example> GET api/HostingCost/0 </example>
+        /// <example> GET api/HostingCost/14 </example>
+        /// <example> GET api/HostingCost/15 </example>
+        /// <example> GET api/HostingCost/21 </example>
+        /// <example> GET api/HostingCost/28 </example>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> fn+"fortnights at $ 5.50/FN= "+amount+" CAD","HST 13%= $"+HST+" CAD","Total= $"+Total+" CAD" </returns>
+        public IEnumerable<string> HostingCost(double id)
+        {
+            double fn=Math.Round(Math.Floor(id/14)+1),2);
+            double amount=Math.Round(5.50*fn,2);
+            double HST=Math.Round((amount*13)/100,2));
+            double Total=amount+HST;
+            return new string[] {fn+"fortnights at $ 5.50/FN= "+amount+" CAD","HST 13%= $"+HST+" CAD","Total= $"+Total+" CAD"};
+        }
+    }
+}
 
